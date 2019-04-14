cb6350703f7fccad50ec95337698c207990c9905
Nicole Dow <nikki.dow71@gmail.com>
2019-04-14 19:39:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array 
+  my_array = ["Nikki", "mom", "dad", "Sammi"]
+  return my_array
 end
 
 def add_element_to_end_of_array(array, element)
